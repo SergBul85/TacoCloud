c87081448d846bf0e4a44ae7b8265d0e502cd921
SergBul85 <donetsk55555@gmail.com>
2026-05-20 20:26:49 +0300

    P_2.2_69

---
+++ src/main/java/sia/tacos/web/OrderController.java
@@ -0,0 +1,27 @@
+package sia.tacos.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+import sia.tacos.TacoOrder;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm() {
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+        log.info("order submitted: {}", order);
+        sessionStatus.setComplete();
+        return "redirect:/";
+    }
+
+}
